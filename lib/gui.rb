af4211a06daf43bc170d92b34f0527b73f57354d
Francis Vo <firefreakfrancis@gmail.com>
2011-01-28 13:00:18 -0800

    fixed gui

--- lib/gui.rb
+++ lib/gui.rb
@@ -6,16 +6,20 @@ require 'board'
 class Gui  < Processing::App
 	#attr_accessor :size, :stones, :b
 	def setup
-	smooth
+		smooth
     no_stroke
     frame_rate 30
     @stones = 19
+		@turn = :black
 		@b = Board.new(@stones)
-		@b.add_stone(:black,0,0)
+		#@b.add_stone(:black,0,0)
 		@size = (width < height ? width : height)/@stones
 	end
 
 	def draw
+		fill(255,202,122)
+		rect(0,0,width, height)
+		fill(255)
 		z = @size/5
 		0.upto(@b.size-1){|x|
 			0.upto(@b.size-1){|y|
@@ -33,14 +37,41 @@ class Gui  < Processing::App
 				end
 				if @b.board[x][y] == :black then
 					fill(0)
-					ellipse(y*@size+@size/2,y*@size+@size/2, 5*@size/6,5*@size/6)
+					ellipse(y*@size+@size/2,x*@size+@size/2, 5*@size/6,5*@size/6)
 					fill(255)
 				elsif @b.board[x][y] == :white then
-
+					fill(255)
+					stroke(0)
+					ellipse(y*@size+@size/2,x*@size+@size/2, 5*@size/6,5*@size/6)
+					no_stroke
+					fill(255)
 				end
 			}
-		}
+		}	
+		y = mouseX/@size
+		x = mouseY/@size
+
+		if @b.board[x][y] == :empty then
+			if @turn == :black then
+				fill(0,150)
+				ellipse(y*@size+@size/2,x*@size+@size/2, 5*@size/6,5*@size/6)
+				fill(255)
+			elsif @turn == :white then
+				fill(255,150)
+				ellipse(y*@size+@size/2,x*@size+@size/2, 5*@size/6,5*@size/6)
+				fill(255)
+			end
+		end 
 	end
+	def mouseClicked
+		y = mouseX/@size
+		x = mouseY/@size
+
+		if @b.add_stone(@turn,x,y) then
+			@turn = (@turn == :black ? :white : :black)
+		end
+	end
+
 end
 
 Gui.new(:width => 800, :height => 800, :title => "Go")
